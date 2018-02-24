00b8d74eb4a648edeeaa579ca088cc22c299df32
chldbtjd2272 <chldbtjd2272@naver.com>
2018-02-24 17:30:38 +0900

    no message

--- routes/login/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-var signup= require('./signup');
-var signin = require('./signin');
-router.use('/signup',signup);
-router.use('/signin', signin);
-
-
-module.exports = router;
